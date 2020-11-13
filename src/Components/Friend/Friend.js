656fe8f4983a8c0cc3997cda5ced136b96ba5cb6
Arkadii <asharaev210497@gmail.com>
2020-11-13 14:49:30 +0700

    work with friends page

--- src/Components/Friend/Friend.js
+++ src/Components/Friend/Friend.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Friend.css'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import Radio from "@material-ui/core/Radio";
 
 const Friend = () => {
+    const [selectedValue='1', setSelectedValue] = useState();
+
+    const handleChange = (event) => {
+        setSelectedValue(event.target.value);
+    };
     return (
         <div className="friend">
             <img src="https://images.unsplash.com/photo-1604158882729-a9fcaa32d25e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1354&q=80" alt="" className="friend__header"/>
@@ -10,20 +18,60 @@ const Friend = () => {
                 <h2>Nicholas Grissom</h2>
                 <h6>San Francisco, CA</h6>
             </hgroup>
-            <ul className="friend__list">
-                <li>
-                    <p>52</p>
-                    <p>Friends</p>
-                </li>
-                <li>
-                    <p>240</p>
-                    <p>Photos</p>
-                </li>
-                <li>
-                    <p>16</p>
-                    <p>Videos</p>
-                </li>
-            </ul>
+
+
+                  <div className="friend__container">
+                      <div className={selectedValue=== '1' ? "friend__mainPage" :"inactive"}>
+                          <ul className="friend__list">
+                              <li>
+                                  <h3>52</h3>
+                                  <p>Friends</p>
+                              </li>
+                              <li>
+                                  <h3>240</h3>
+                                  <p>Photos</p>
+                              </li>
+                              <li>
+                                  <h3>16</h3>
+                                  <p>Videos</p>
+                              </li>
+                          </ul>
+                          <div className="friend__icons">
+                              <InsertEmoticonIcon/>
+                              <QuestionAnswerIcon/>
+                          </div>
+                      </div>
+                      <div className={selectedValue=== '2' ? "friend__description" :"inactive"}>
+                          <p>Hi , I'm Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+                          <p className="friend__descriptionDate">Friend Since:</p>
+                          <h3>December 2014</h3>
+                      </div>
+              </div>
+
+
+
+
+
+
+
+            <div className="friend__switchButtons">
+                <Radio
+                    checked={selectedValue === "1"}
+                    onChange={handleChange}
+                    value="1"
+                    name="radio-button-demo"
+                    inputProps={{ "aria-label": "1" }}
+                />
+                <Radio
+                    checked={selectedValue === "2"}
+                    onChange={handleChange}
+                    value="2"
+                    name="radio-button-demo"
+                    inputProps={{ "aria-label": "2" }}
+                    size="small"
+                />
+
+            </div>
         </div>
     );
 };
--- src/Components/FullFriends/FullFriends.js
+++ src/Components/FullFriends/FullFriends.js
@@ -4,12 +4,18 @@ import './FullFriends.css'
 const FullFriends = () => {
     return (
         <div className="fullFriends">
-            <Friend/>
-            <Friend/>
-            <Friend/>
-            <Friend/>
-            <Friend/>
-            <Friend/>
+            <header className="fullFriends__header">
+                <h4>James's Friends 86</h4>
+                <input type="search" placeholder="Search Friends..."/>
+            </header>
+           <div className="fullFriends__container">
+               <Friend/>
+               <Friend/>
+               <Friend/>
+               <Friend/>
+               <Friend/>
+               <Friend/>
+           </div>
         </div>
     );
 };
