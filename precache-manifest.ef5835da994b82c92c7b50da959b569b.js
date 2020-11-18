38c0c2ed03172013bcb7415630d9cd24d752cf93
Arkadii <asharaev210497@gmail.com>
2020-11-18 16:08:09 +0700

    Updates

---
+++ precache-manifest.ef5835da994b82c92c7b50da959b569b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a9a780b0c15ecfc165f07beee3c11bc",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "a1a2ceb7ced6bc652d73",
+    "url": "/social-network/static/css/main.6561318e.chunk.css"
+  },
+  {
+    "revision": "c20f1aab148fd43522e0",
+    "url": "/social-network/static/js/2.510d2639.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/social-network/static/js/2.510d2639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a2ceb7ced6bc652d73",
+    "url": "/social-network/static/js/main.183ac582.chunk.js"
+  },
+  {
+    "revision": "73dc02b1c42065a1d3d3",
+    "url": "/social-network/static/js/runtime-main.a948e851.js"
+  }
+]);
